9b8503575441d04ec21ccf8ee91fc34585ff01a3
GreatEki <ekeneidiagbor@gmail.com>
2023-04-18 16:42:08 +0100

    feat: impl create user

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import router from "./router";
+
+import { errorHandler, NotFoundError } from "@greateki-ticket-ms-demo/common";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/", router);
+
+app.all("*", async (req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError("Resource not found"));
+});
+
+app.use(errorHandler);
 
 export default app;
--- lib/user/user.controller.ts
+++ lib/user/user.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import prisma from "../../config/prisma-client";
+import { BadRequestError } from "@greateki-ticket-ms-demo/common";
+import bcrypt from "bcrypt";
 
 export const createUser = async (
   req: Request,
@@ -6,7 +9,32 @@ export const createUser = async (
   next: NextFunction
 ) => {
   try {
-    res.send("This is create user route");
+    const { name, email, password, age } = req.body;
+
+    // check if email exist
+    const userExists = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userExists) throw new BadRequestError("Email is already taken");
+
+    const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPass,
+        age,
+      },
+    });
+
+    return res.json({
+      status: "Created",
+      statusCode: 201,
+      message: "User created",
+      data: newUser,
+    });
   } catch (err) {
     next(err);
   }
